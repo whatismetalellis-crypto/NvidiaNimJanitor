9264735ade62911d638ceff8f9354f48a0caca95
whatismetalellis-crypto <whatismetalellis@gmail.com>
2026-07-03 04:44:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ const MODEL_MAPPING = {
   'GLM': 'z-ai/glm-5.1',
   'deepseek-v4-pro': 'deepseek-ai/deepseek-v4-pro',
   'model': 'moonshotai/kimi-k2.6',
-  'model': 'deepseek-ai/deepseek-v4-flash'
+  'model': 'deepseek-ai/deepseek-v4-flash',
+  'model': 'z-ai/glm-5.2'
 };
 
 // Root endpoint
